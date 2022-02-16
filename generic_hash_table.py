6865c721e5382f21e320c2535af6c59f918f0967
Cyrus <cyrus@Cyruss-MacBook-Pro.local>
2022-02-16 14:32:48 -0700

    second commit

--- generic_hash_table.py
+++ generic_hash_table.py
@@ -59,7 +59,12 @@ if __name__ == "__main__":
     H[1] = 'one'
     H[2] = 'two'
     H[3] = 'three'
+    H[4] = 'four'
+    H[5] = 'five'
 
     print(H[1])
     print(H[2])
     print(H[3])
+    print(H[4])
+    print(H[5])
+
